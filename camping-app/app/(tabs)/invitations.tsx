82ccba82f35624be6feb5b9d33b798149b447a9e
Alafajraoui <fullstackjs110299@gmail.com>
2024-07-24 16:06:21 +0200

    new frontend structure with navigations

---
+++ camping-app/app/(tabs)/invitations.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const invitations = () => {
+  return (
+    <View>
+      <Text>invitations</Text>
+    </View>
+  )
+}
+
+export default invitations
+
+const styles = StyleSheet.create({})
