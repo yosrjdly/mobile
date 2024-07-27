b9e88aa1f2e08bb4ba1426068edcb4ce382fda89
Alafajraoui <fullstackjs110299@gmail.com>
2024-07-27 13:29:19 +0200

    Interests layout done

--- camping-app/app/auth/SignUp.tsx
+++ camping-app/app/auth/SignUp.tsx
@@ -3,9 +3,8 @@ import { StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity, K
 import { AntDesign } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { useRouter } from 'expo-router';
-
 import axios from 'axios';
-import { router } from 'expo-router';
+
 
 const RegisterScreen = () => {
   const [name, setName] = useState('');
@@ -30,7 +29,7 @@ const RegisterScreen = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await axios.post('http://127.0.0.1:5000/api/users/register', {
+      const response = await axios.post('http://172.19.3.206:5000/api/users/register', {
         name,
         email,
         password,
@@ -38,7 +37,7 @@ const RegisterScreen = () => {
       });
       console.log("success")
       Alert.alert('Success', response.data.message);
-      router.replace('auth/SignIn');
+      router.replace('UserInterests/Interests');
     } catch (error: unknown) {
       if (axios.isAxiosError(error)) {
         console.error(error.response?.data || error.message);
