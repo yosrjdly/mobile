f576816b67791287617f2ffedbf3e35f91137258
Alafajraoui <fullstackjs110299@gmail.com>
2024-07-28 20:32:22 +0200

    hiding header for the login and register screens

--- camping-app/app/_layout.tsx
+++ camping-app/app/_layout.tsx
@@ -6,8 +6,8 @@ export default function RootLayout() {
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{headerShown: false }} />
-      <Stack.Screen name="auth/SignIn" options={{ title: 'Sign In' }} />
-      <Stack.Screen name="auth/SignUp" options={{ title: 'Sign Up' }} />
+      <Stack.Screen name="auth/SignIn" options={{ title: 'Sign In' , headerShown:false}} />
+      <Stack.Screen name="auth/SignUp" options={{ title: 'Sign Up' , headerShown: false}} />
       <Stack.Screen name="UserInterests/Interests" options={{title: "Interests", headerShown: false}} />
     </Stack>
   );
