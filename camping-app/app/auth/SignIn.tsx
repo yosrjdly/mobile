e1fb1e9fc1a62b83dfc2d698c1ad4e9d09e544a8
yosrjdly <yosrjdly@gmail.com>
2024-07-28 19:48:34 +0100

    adjustememnt on profile

--- camping-app/app/auth/SignIn.tsx
+++ camping-app/app/auth/SignIn.tsx
@@ -35,11 +35,7 @@ const LoginScreen = () => {
 
 
       // Example: Replace with your actual login API endpoint
-      const res = await fetch('http://192.168.1.103:5000/api/users/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-
+    
       const res = await fetch('http://192.168.1.112:5000/api/users/login', {
 
         method: 'POST',
