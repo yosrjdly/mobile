b1c1932983fd30852a208e9ee561d27c6e8c9cbf
lobna3 <yousfi55lobna@gmail.com>
2024-08-01 18:00:36 +0200

    add

--- camping-app/app/[postId]/index.tsx
+++ camping-app/app/[postId]/index.tsx
@@ -76,7 +76,7 @@ const PostDetailScreen: React.FC = () => {
   const joinPost = async (body: JoinCampingPost) => {
     try {
 
-      const response = await axios.post('http://192.168.10.18:5000/api/joinPosts/add', body);
+      const response = await axios.post('http://192.168.10.20:5000/api/joinPosts/add', body);
 
       console.log('Success', response.data.data);
       setIsSuccessModalVisible(true); // Show success modal
@@ -89,7 +89,7 @@ const PostDetailScreen: React.FC = () => {
 
   const cancelPost = async (body: JoinCampingPost) => {
     try {
-      const response = await axios.post('http://192.168.10.18:5000/api/joinPosts/cancel', body);
+      const response = await axios.post('http://192.168.10.20:5000/api/joinPosts/cancel', body);
 
       console.log('Success', response.data);
       setIsCancelSuccessModalVisible(true); // Show cancellation success modal
@@ -105,7 +105,7 @@ const PostDetailScreen: React.FC = () => {
       setLoading(true);
       try {
 
-        const response = await axios.get<ApiResponse>(`http://192.168.10.18:5000/api/camps/${id}`);
+        const response = await axios.get<ApiResponse>(`http://192.168.10.20:5000/api/camps/${id}`);
 
         setPost(response.data.data);
         console.log(response.data.data);
--- camping-app/app/auth/SignIn.tsx
+++ camping-app/app/auth/SignIn.tsx
@@ -40,7 +40,7 @@ const LoginScreen = () => {
      
 
 
-      const res = await fetch('http://192.168.10.18:5000/api/users/login', {
+      const res = await fetch('http://192.168.10.20:5000/api/users/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(newData)
