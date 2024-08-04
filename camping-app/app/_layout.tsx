07aafe5d98ab95d74e7c7c26df873859b5537f7f
Alafajraoui <fullstackjs110299@gmail.com>
2024-08-04 03:55:42 +0200

    adjusting the layout of the homescreen + add a slide screen in the home screen to handle Logout,MyProfile,MyCamps,Aboutus,favorite and their navigation+ layout of each screen

--- camping-app/app/_layout.tsx
+++ camping-app/app/_layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout() {
       <Stack.Screen name="creatCamp/CreateCamPost" options={{title: "Interests", headerShown: false}} />
       <Stack.Screen name="[postId]/index" options={{title: "Interests", headerShown: false}} />
       <Stack.Screen name="experience/experience" options={{title: "experience", headerShown: false}} />
+      <Stack.Screen name="aboutUs/AboutUs" options={{title:"AboutUs" , headerShown: false}}/>
     </Stack>
   );
 }
